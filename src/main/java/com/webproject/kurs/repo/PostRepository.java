e778fa0380bb90ed63b537678a88f5f2bb254d7b
petrovskkky <petrovskiij2@gmail.com>
2023-02-20 19:06:53 +0300

    Add all before Bootstrap learning

---
+++ src/main/java/com/webproject/kurs/repo/PostRepository.java
@@ -0,0 +1,7 @@
+package com.webproject.kurs.repo;
+
+import com.webproject.kurs.models.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+}
