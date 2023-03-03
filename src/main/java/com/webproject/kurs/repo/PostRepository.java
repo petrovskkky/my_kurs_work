1d3441e5c6cf8df3b85aacad61210c47026b3593
petrovskkky <petrovskiij2@gmail.com>
2023-03-03 11:47:41 +0300

    Cosmetic edits

--- src/main/java/com/webproject/kurs/repo/PostRepository.java
+++ src/main/java/com/webproject/kurs/repo/PostRepository.java
@@ -1,7 +1,6 @@
 package com.webproject.kurs.repo;
 
 import com.webproject.kurs.models.Post;
-import com.webproject.kurs.models.PostSick;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
--- src/main/java/com/webproject/kurs/repo/PostSickRepository.java
+++ src/main/java/com/webproject/kurs/repo/PostSickRepository.java
@@ -1,6 +1,5 @@
 package com.webproject.kurs.repo;
 
-import com.webproject.kurs.models.Post;
 import com.webproject.kurs.models.PostSick;
 import org.springframework.data.repository.CrudRepository;
 
